63a9952fc44ec4e0e0c50ff092c49f40766ee1c4
Santiago Sierra Cano <santiscano@gmail.com>
2024-02-28 12:22:00 -0500

    update campaings manual 202403

--- src/docs/novaventa.ts
+++ src/docs/novaventa.ts
@@ -4,6 +4,6 @@ export const requestData = {
   body: {
     login: "admin@SOLUCIONES.com", 
     password: "Admin@SOLUCIONES.com", 
-    campaings: [ "202402", "202403", "202404" ]
+    campaings: [ "202401", "202402", "202403" ]
   }
 }
