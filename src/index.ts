fd799257a70e50808b5f532ccbbb1ceeb594be05
Santiago Sierra Cano <santiscano@gmail.com>
2024-08-08 15:17:47 -0500

    chore: update cronjob execution interval to 30 minutes

--- src/index.ts
+++ src/index.ts
@@ -5,11 +5,15 @@ import RunValidateCampaign from "./services/RunValidateCampaign";
 
 // Función para calcular los minutos de ejecución
 function calculateIntervals(numItems: number): number[] {
-  const interval = Math.floor(60 / numItems); // Calcular el intervalo en minutos
+  const interval = Math.floor(30 / numItems); // Calcular el intervalo en minutos
   const intervals = [];
   
   for (let i = 0; i < numItems; i++) {
     intervals.push(i * interval + 1); // +1 para evitar ejecutar en el minuto 0
+  }
+
+  for (let i = 0; i < numItems; i++) {
+    intervals.push(i * interval + 31); // +31 para ejecutar en la segunda mitad de la hora
   }
 
   return intervals;
@@ -22,18 +26,14 @@ function scheduleCrons(listCedisActive: any[]) {
 
   intervals.forEach((minute, index) => {
     cron.schedule(`${minute} * * * *`, () => {
-      console.log(`Ejecutando tarea para CEDI ${listCedisActive[index]}`);
-      // Aquí puedes llamar la función específica que quieras ejecutar
-      // RunScrapps.bucleScrapp(listCedisActive[index]);
-      // RunValidateCampaign.bucleValidateCampaign(listCedisActive[index]);
+      console.log(`Ejecutando tarea para CEDI ${JSON.stringify(listCedisActive[index])}`);
       RunScrapps.runForCedi(listCedisActive[index]);
       RunValidateCampaign.runForCedi(listCedisActive[index]);
     });
   });
 }
 
-// Programar las tareas cron cada hora
-cron.schedule('0 * * * *', async () => {
+async function startScrapp() {
   const listCedisActive = await RunScrapps.getCedis(); // Obtener la lista de CEDIS activos
   console.log("Cantidad de CEDIS activos:", listCedisActive.length);
   
@@ -41,15 +41,9 @@ cron.schedule('0 * * * *', async () => {
   cron.getTasks().forEach((task) => task.stop());
   
   scheduleCrons(listCedisActive);
-});
-
-async function startScrapp() {
-  const listCedisActive = await RunScrapps.getCedis(); // Obtener la lista de CEDIS activos
-  console.log("start Cantidad de CEDIS activos:", listCedisActive.length);
-  
-  // Limpiar tareas cron previas
-  cron.getTasks().forEach((task) => task.stop());
-  
-  scheduleCrons(listCedisActive);
 }
+
+// Programar las tareas cron cada hora
+cron.schedule('0 * * * *', startScrapp );
+
 startScrapp();
